dca26f8dadb780026e2349e70461775314a6dbe0
alex <nalleballeuwu@gmail.com>
2025-02-04 11:03:56 +0100

    added enum categories

--- src/main/java/com/example/feature_branches/models/Category.java
+++ src/main/java/com/example/feature_branches/models/Category.java
@@ -17,4 +17,20 @@ public class Category {
     @Enumerated(EnumType.STRING)
     @Column(name = "category")
     private Categories categories;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Categories getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Categories categories) {
+        this.categories = categories;
+    }
 }
