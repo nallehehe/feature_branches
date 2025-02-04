acf62cb93acc99aa901d8d164daa4fdaebd87ae2
alex <nalleballeuwu@gmail.com>
2025-02-04 09:11:11 +0100

    first commit

---
+++ src/main/java/com/example/feature_branches/FeatureBranchesApplication.java
@@ -0,0 +1,13 @@
+package com.example.feature_branches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureBranchesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureBranchesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/feature_branches/FeatureBranchesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.feature_branches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureBranchesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
